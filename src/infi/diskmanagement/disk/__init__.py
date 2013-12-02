4169fbbaec08e928eb28a04165ee4ad7d501661b
Guy Rozendorn <guy@rzn.co.il>
2013-12-02 23:35:42 +0200

    HIP-684 ioctl_disk_update_drive_size

--- src/infi/diskmanagement/disk/__init__.py
+++ src/infi/diskmanagement/disk/__init__.py
@@ -230,11 +230,14 @@ class Partition(object):
         return Volume.get_from_disk_and_partition(self._disk, self)
 
     def resize(self, size_in_bytes):
+        self._disk._io.ioctl_disk_update_drive_size()
+
         _struct = structures.DISK_GROW_PARTITION()
         _struct.PartitionNumber = self._struct.PartitionNumber
         _struct.BytesToGrow = to_large_integer(size_in_bytes - self.get_size_in_bytes())
         self._disk._io.ioctl_disk_grow_partition(_struct)
         self._struct.PartitionLength = to_large_integer(size_in_bytes)  # we get_size_in_bytes will return the new size
+
 
 class Disk(object):
     def __init__(self, disk_number):
